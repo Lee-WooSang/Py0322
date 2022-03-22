c51e7047f31de81a29ffc35cd62a3399e581e9c0
LWS <modulove1972@gmail.com>
2022-03-22 14:14:35 +0900

    aaa

--- ifelse.py
+++ ifelse.py
@@ -5,10 +5,23 @@ score = int(input("점수를 입력:"))
 if 90 <= score <= 100:
     grade = "A"
 elif 80 <= score < 90:
-    grde = "B"
-elif 70 <= socre < 80:
+    grade = "B"
+elif 70 <= score < 80:
     grade = "C"
 else:
     grade = "D"
 
 print("등급은 ", grade)
+
+# 반복구문
+value = 5
+while value > 0:
+    print(value)
+    value -= 1
+
+print("---실행종료---")
+
+# 시퀀스 형식
+for i in ["100", "apple", 3.14]:
+    print(i, type(i))
+
