5cbe68d54c94ba150519d3b3594cf5b666b6c8bb
LWS <modulove1972@gmail.com>
2022-03-22 15:05:41 +0900

    break20

--- Demobreak.py
+++ Demobreak.py
@@ -2,7 +2,21 @@
 
 lst = [1,2,3,4,5,6,7,8,9,10]
 
+# for item in lst:
+#     if item > 5:
+#         break
+#     print("item:{0}".format(item))
+
+
+result = [item**2 for item in lst if item > 5]
+print(result)
+
+lst2=[]
 for item in lst:
     if item > 5:
-        break
-    print("item:{0}".format(item))
+        lst2.append(item**2)
+print(lst2)
+
+colors = {100:"apple", 200:"banana", 300:"kiwi"}
+
+print([v.upper() for v in colors.values()])
